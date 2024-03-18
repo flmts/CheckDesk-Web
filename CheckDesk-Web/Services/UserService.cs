45c671b314ccebc47c49c9d1b44cf8aff0b734b4
flmts <florian.montastier@outlook.fr>
2024-03-18 09:55:00 +0100

    ajout service user (utilisation API)

---
+++ CheckDesk-Web/Services/UserService.cs
@@ -0,0 +1,24 @@
+﻿using CheckDesk_Web.Data;
+
+namespace CheckDesk_Web.Services;
+
+
+public class UserService
+{
+        private readonly HttpClient _httpClient;
+
+        public UserService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<User>> GetUsersAsync()
+        {
+            var response = await _httpClient.GetAsync("http://localhost:5000/users");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<User>>(content);
+        }
+}
+
